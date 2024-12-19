461d7b12542e598c63afdd7096430322775762b3
TaoZeyu <i@taozeyu.com>
2024-12-19 13:33:50 +0800

    feat(block): download by stream mode

--- tasks/read-url/main.py
+++ tasks/read-url/main.py
@@ -1,8 +1,11 @@
+import io
 import json
 import requests
 import urllib.parse
 
-def main(inputs: dict):
+from oocana import Context
+
+def main(inputs: dict, context: Context):
   url = inputs["url"]
   query: dict[str, str] = inputs["query"]
   headers: dict[str, str] = inputs["headers"]
@@ -11,7 +14,32 @@ def main(inputs: dict):
     query_string = urllib.parse.urlencode(query)
     url = f"{url}?{query_string}"
 
-  response = requests.get(url, headers=headers)
+  buffer = io.BytesIO()
+  response = requests.get(url, headers=headers, stream=True)
+  _raise_if_fail(response)
+
+  content_len = _content_length(response)
+  buffer_len: int = 0
+
+  for chunk in response.iter_content(chunk_size=16384):
+    if chunk is not None:
+      buffer.write(chunk)
+      buffer_len += len(chunk)
+      if content_len is not None:
+        context.report_progress(float(buffer_len) / float(content_len) * 100.0)
+
+  return { "binary": buffer.getvalue() }
+
+def _content_length(response: requests.Response) -> int | None:
+  try:
+    content_length = response.headers.get("Content-Length", None)
+    if content_length is None:
+      return None
+    return int(content_length)
+  except Exception:
+    return None
+
+def _raise_if_fail(response: requests.Response):
   if response.status_code != 200:
     error: str
     if "application/json" in response.headers.get("Content-Type", ""):
@@ -22,5 +50,3 @@ def main(inputs: dict):
       raise Exception(f"Fetch URL failed with status code {response.status_code}")
     else:
       raise Exception(f"Fetch URL failed with status code: {response.status_code}: {error}")
-
-  return { "binary": response.content }
