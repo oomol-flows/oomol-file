c5e01071f496efbd33d5b74253c0a3573553f5fc
TaoZeyu <i@taozeyu.com>
2025-01-15 09:31:02 +0800

    feat(block): add more fields for read folder

--- tasks/read-folder/__init__.py
+++ tasks/read-folder/__init__.py
@@ -1,21 +1,48 @@
+from ntpath import isdir
 import os
 
 def main(params: dict):
   folder_path: str = params["folder"]
-  only_file_name: bool = params["only_file_name"]
+  sort: bool = params["sort"]
+  traverse: bool = params["traverse"]
+  absolute_path: bool = params["absolute_path"]
+
+  if not os.path.exists(folder_path) or \
+     not os.path.isdir(folder_path):
+    return {
+      "files": [],
+      "folder_exists": False,
+    }
   files: list[str] = []
   folder_exists = False
 
-  if os.path.exists(folder_path) and \
-     os.path.isdir(folder_path):
-    folder_exists = True
-    for file in os.listdir(folder_path):
-      if only_file_name:
-        files.append(file)
-      else:
-        files.append(os.path.join(folder_path, file))
+  _search(folder_path, "", files, traverse)
+
+  if sort:
+    files.sort()
+
+  if absolute_path:
+    for i in range(len(files)):
+      files[i] = os.path.join(folder_path, files[i])
 
   return { 
     "files": files,
     "folder_exists": folder_exists,
   }
+
+def _search(root_path: str, sub_path: str, files: list[str], traverse: bool):
+  folder_path: str = root_path
+  if sub_path != "":
+    folder_path = os.path.join(root_path, sub_path)
+
+  for file in os.listdir(folder_path):
+    abs_file = os.path.join(folder_path, file)
+    next_path: str
+    if sub_path == "":
+      next_path = file
+    else:
+      next_path = os.path.join(sub_path, file)
+
+    files.append(next_path)
+    if traverse and os.path.isdir(abs_file):
+      _search(root_path, next_path, files, traverse)
