fbec47ef67e50419ef9e552bd26fb983405d5794
TaoZeyu <i@taozeyu.com>
2025-01-15 10:35:00 +0800

    feat(block): add more fields for download URL block

--- tasks/read-url/__init__.py
+++ tasks/read-url/__init__.py
@@ -1,21 +1,36 @@
 import io
 import json
-import requests
-import urllib.parse
 
 from oocana import Context
+from typing import cast
+from urllib.parse import urlencode
+from requests import Session, Response
+from requests.adapters import HTTPAdapter, Retry
 
 def main(params: dict, context: Context):
   url = params["url"]
   query: dict[str, str] = params["query"]
   headers: dict[str, str] = params["headers"]
+  timeout: int | float | None = params["timeout"]
+  retry_times: int = params["retry_times"]
 
   if len(query) > 0:
-    query_string = urllib.parse.urlencode(query)
+    query_string = urlencode(query)
     url = f"{url}?{query_string}"
 
+  if timeout == 0:
+    timeout = None
+  else:
+    timeout = cast(int, timeout) * 1000.0
+
   buffer = io.BytesIO()
-  response = requests.get(url, headers=headers, stream=True)
+  session = _session(retry_times)
+  response = session.get(
+    url, 
+    headers=headers, 
+    stream=True, 
+    timeout=timeout,
+  )
   _raise_if_fail(response)
 
   content_len = _content_length(response)
@@ -28,9 +43,12 @@ def main(params: dict, context: Context):
       if content_len is not None:
         context.report_progress(float(buffer_len) / float(content_len) * 100.0)
 
-  return { "binary": buffer.getvalue() }
+  context.report_progress(100.0)
+  binary = buffer.getvalue()
 
-def _content_length(response: requests.Response) -> int | None:
+  return { "binary": binary }
+
+def _content_length(response: Response) -> int | None:
   try:
     content_length = response.headers.get("Content-Length", None)
     if content_length is None:
@@ -39,7 +57,7 @@ def _content_length(response: requests.Response) -> int | None:
   except Exception:
     return None
 
-def _raise_if_fail(response: requests.Response):
+def _raise_if_fail(response: Response):
   if response.status_code != 200:
     error: str
     if "application/json" in response.headers.get("Content-Type", ""):
@@ -50,3 +68,16 @@ def _raise_if_fail(response: requests.Response):
       raise Exception(f"Fetch URL failed with status code {response.status_code}")
     else:
       raise Exception(f"Fetch URL failed with status code: {response.status_code}: {error}")
+
+def _session(retry_times: int):
+  session = Session()
+  retries = Retry(
+    total=retry_times,
+    backoff_factor=0.1,
+    status_forcelist=[502, 503, 504],
+  )
+  session.mount(
+    prefix="https://",
+    adapter=HTTPAdapter(max_retries=retries),
+  )
+  return session
