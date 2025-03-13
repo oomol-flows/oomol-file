e7cb7fdc603091edc0322b140512437a88c753c8
TaoZeyu <i@taozeyu.com>
2025-03-13 16:36:58 +0800

    refactor(tasks): rename to "copy folder"

--- tasks/copy_folder/main.ts
+++ tasks/copy_folder/main.ts
@@ -3,30 +3,30 @@ import fs from "fs-extra"
 import path from "path";
 
 type Inputs = {
-  source_dir: string;
-  destination_dir: string | null;
+  source_folder: string;
+  destination_folder: string | null;
 }
 type Outputs = {
-  destination_dir: string;
+  destination_folder: string;
 }
 
 export default async function (
   params: Inputs,
   context: Context<Inputs, Outputs>
 ): Promise<Outputs> {
-  const { source_dir, destination_dir } = params;
-  let target_dir = destination_dir;
+  const { source_folder } = params;
+  let { destination_folder } = params;
   
-  if (target_dir === null) {
-    target_dir = path.join(context.sessionDir, context.jobId);
+  if (destination_folder === null) {
+    destination_folder = path.join(context.sessionDir, context.jobId);
   }
   try {
-    await fs.copy(source_dir, target_dir);
+    await fs.copy(source_folder, destination_folder);
     console.log('Folder copied successfully!');
   } catch (err) {
     console.error('Error copying folder:', err);
     throw err;
   }
 
-  return { destination_dir: target_dir };
+  return { destination_folder };
 };
