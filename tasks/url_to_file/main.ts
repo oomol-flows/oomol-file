bee3189d61094c5618ec5afd047696c52a06064f
TaoZeyu <i@taozeyu.com>
2025-06-19 08:44:10 +0800

    feat(tasks): add ext name into path

--- tasks/url_to_file/main.ts
+++ tasks/url_to_file/main.ts
@@ -18,19 +18,51 @@ type Outputs = {
 };
 //#endregion
 
-export default async function (
-  params: Inputs,
-  context: Context<Inputs, Outputs>
-): Promise<Outputs> {
-  const fileNameWithExt = path.basename(params.url);
-  const filePath = path.join(context.sessionDir, fileNameWithExt);
-  const folderPath = path.dirname(filePath);
+export default async function (params: Inputs, context: Context<Inputs, Outputs>): Promise<Outputs> {
+  let [fileName, extName] = splitExtNames(path.basename(params.url));
+
+  if (extName === null) {
+    extName = params.ext_name;
+  }
+  if (extName !== null) {
+    extName = extName.replace(/^\./, "");
+    fileName = `${fileName}.${extName}`;
+  }
+  const folderPath = path.join(context.sessionDir, "downloading");
   const binary = await download({
     ...params,
     reportProgress: p => context.reportProgress(p),
   });
+  let filePath = path.join(folderPath, fileName);
+
+  if (await fileExists(filePath)) {
+    fileName = context.jobId;
+    if (extName != null) {
+      fileName = `${fileName}.${extName}`;
+    }
+    filePath = path.join(folderPath, fileName);
+  }
   await fs.mkdir(folderPath, { recursive: true });
   await fs.writeFile(filePath, binary);
 
   return { file_path: filePath };
+}
+
+function splitExtNames(fileName: string): [string, string | null] {
+  const cells = fileName.split(".");
+  if (cells.length >= 2) {
+    const extName = cells.pop();
+    return [cells.join("."), extName];
+  } else {
+    return [fileName, null];
+  }
+}
+
+async function fileExists(filePath: string): Promise<boolean> {
+  try {
+    await fs.stat(filePath);
+    return true;
+  } catch {
+    return false;
+  }
 }
